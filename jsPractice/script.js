abeac91744a6f1072a23b28cef8ddafce2a37d5b
nileshgarg0023 <nileshgarg0023@gmail.com>
2024-03-29 10:36:46 +0530

    project 4 completed

--- jsPractice/script.js
+++ jsPractice/script.js
@@ -1,137 +1,62 @@
 // let btn = document.querySelector("#btn");
-// let para = document.querySelector("#mainP");
 
-// btn.addEventListener("click", () => {
-//   para.textContent = "text changed";
+// let input = document.querySelector("input");
+
+// let data = [
+//   {
+//     name: "harsh",
+//     src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D",
+//   },
+//   {
+//     name: "nilu",
+//     src: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D",
+//   },
+//   {
+//     name: "pilu",
+//     src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3xlbnwwfHwwfHx8MA%3D%3D",
+//   },
+//   {
+//     name: "gilu",
+//     src: "https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBwaWN8ZW58MHx8MHx8fDA%3D",
+//   },
+// ];
+
+// var pers = "";
+
+// data.forEach((elem) => {
+//   pers += `<div class="person">
+//     <div class="img">
+//         <img src=${elem.src}
+//             alt="">
+//     </div>
+//     <h4>${elem.name}</h4>
+// </div>`;
 // });
 
-// let img1 = document.querySelector(".image1");
-// let img2 = document.querySelector(".image2");
+// document.querySelector(".people").innerHTML = pers;
 
-// document.querySelector(".swapbtn").addEventListener("click", () => {
-//   let src1 = img1.src;
-//   let src2 = img2.src;
-
-//   img1.src = src2;
-//   img2.src = src1;
-// });
-
-// let form = document.querySelector("form");
-// let input1 = document.querySelectorAll('input[type="text"]');
-// let heading = document.querySelector("h4");
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   //   if (input1.value === "" || input2.value === "") {
-//   //     heading.textContent = "Error! Values cannot be empty";
-//   //     heading.style.color = "red";
-//   //   } else {
-//   //     heading.textContent = "form submitted";
-//   //   }
-
-//   for (let i = 0; i < input1.length; i++) {
-//     if (input1[i].value.trim() === "") {
-//       heading.textContent = "Error, some fields are empty";
-//       heading.style.color = "red";
-//       break;
-//     }
-//   }
-// });
-
-// let add = document.querySelector("#add");
-// let remove = document.querySelector("#remove");
-// let inp = document.querySelector("input");
-// let ul = document.querySelector("ul");
-// let li;
-
-// add.addEventListener("click", () => {
-//   if (inp.value.trim() === "") {
-//   } else {
-//     li = document.createElement("li");
-//     li.textContent = inp.value;
-//     ul.appendChild(li);
-//     inp.value = "";
-//   }
-// });
-
-// remove.addEventListener("click", () => {
-//   ul.removeChild(li);
-// });
-
-// let btn = document.querySelector("#start");
-// let btn2 = document.querySelector("#pause");
-// let btn3 = document.querySelector("#stop");
-// let h3 = document.querySelector("h3");
-// let pause = false;
-// let count = 0;
-// var int;
-
-// btn.addEventListener("click", () => {
-//   pause = false;
-//   int = setInterval(() => {
-//     if (!pause) {
-//       h3.textContent = count;
-//       count++;
-//     }
-//   }, 1000);
-// });
-// btn2.addEventListener("click", () => {
-//   pause = true;
-// });
-// btn3.addEventListener("click", () => {
-//   clearInterval(int);
-// });
-
-// {let home = document.querySelector("#home");
-// let about = document.querySelector("#about");
-// let content = document.querySelector("#content");
-
-// let hometext = document.querySelector("#hometext");
-// let abouttext = document.querySelector("#abouttext");
-// let contenttext = document.querySelector("#contenttext");
-
-// hometext.style.display = "block";
-// hometext.style.width = "50%";
-
-// home.addEventListener("click", () => {
-//   removeall();
-//   hometext.style.display = "block";
-//   hometext.style.width = "50%";
-// });
-// about.addEventListener("click", () => {
-//   removeall();
-//   abouttext.style.display = "block";
-//   abouttext.style.width = "50%";
-// });
-// content.addEventListener("click", () => {
-//   removeall();
-//   contenttext.style.display = "block";
-//   contenttext.style.width = "50%";
-// });
-
-// function removeall() {
-//   document.querySelectorAll("h3").forEach((h3) => {
-//     h3.style.display = "none";
+// input.addEventListener("input", () => {
+//   let matching = data.filter((e) => {
+//     return e.name.startsWith(input.value);
 //   });
-// }}
 
-// let divs = document.querySelectorAll(".tab");
-// let texts = document.querySelectorAll("h3");
-
-// texts[0].style.display = "block";
-// texts[0].style.width = "50%";
-
-// divs.forEach((div, index) => {
-//   div.addEventListener("click", () => {
-//     hideAllText();
-//     texts[index].style.display = "block";
-//     texts[index].style.width = "50%";
+//   var newusers = "";
+//   matching.forEach((elem) => {
+//     newusers += `<div class="person">
+//       <div class="img">
+//           <img src=${elem.src}
+//               alt="">
+//       </div>
+//       <h4>${elem.name}</h4>
+//   </div>`;
 //   });
+
+//   document.querySelector(".people").innerHTML = newusers;
 // });
 
-// function hideAllText() {
-//   texts.forEach((text) => {
-//     text.style.display = "none";
-//   });
-// }
+let textarea = document.querySelector("textarea");
+let counter = document.querySelector("#counter");
+
+textarea.addEventListener("input", () => {
+  counter.textContent = textarea.value.length;
+});
